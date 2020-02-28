46c9714cfb5c0d55bb30bda00f10dadcb9943501
David Morton <davidmorton0@gmail.com>
2020-02-28 12:29:59 +0000

    renders properly

--- src/index.js
+++ src/index.js
@@ -1,12 +1,89 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import marked from 'marked';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var defaultText = `# H1 Heading
+## H2 Heading
+[An inline-style link](https://www.google.com)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+<table>
+  <tr>
+    <td>A Table</td>
+    <td>from</td>
+    <td>inline html</td>
+  </tr>
+</table>
+
+\`Inline code\`
+
+    A code block
+A List:
+1. Item 1
+2. Item 2
+3. Item 3
+
+> A blockquote
+
+![An image](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Markdown Logo")
+
+**bolded text**`;
+
+var markedOptions = {
+  breaks: true
+}
+
+function processHtml(id, text) {
+  document.getElementById(id).innerHTML = marked(text, markedOptions);
+}
+
+class Previewer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      markdown: defaultText,
+      htmlPreview: this.htmlPreview(defaultText),
+      loading: true,
+    };
+  }
+
+  htmlPreview(text) {
+    return marked(text, markedOptions)
+  }
+
+  updateText(event) {
+    this.setState({
+      markdown: event.target.value,
+      htmlPreview: this.htmlPreview(event.target.value),
+    });
+  }
+
+  componentDidMount() {
+    this.setState({
+      loading: false,
+    })
+  }
+
+  componentDidUpdate() {
+    if (!this.state.loading) {
+      processHtml('preview', this.state.markdown)
+    }
+  }
+
+  render() {
+    return (<div>
+              <h2 id="main-h2">Markdown</h2>
+              <textarea id="editor" value={this.state.markdown}
+                onChange={this.updateText.bind(this)}>{defaultText}</textarea>
+              <h2 id="main-h2">HTML</h2>
+              <textarea id="html-preview" value={this.state.htmlPreview}>
+              </textarea>
+              <h2 id="main-h2">Preview</h2>
+            </div>
+           );
+  }
+}
+
+ReactDOM.render(<Previewer />, document.getElementById('app'));
 serviceWorker.unregister();
